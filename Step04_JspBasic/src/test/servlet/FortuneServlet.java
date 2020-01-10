1a15970951d7db560cedebd0dbe9383fe0137e44
Winnie <yeonsu100@gmail.com>
2020-01-10 14:36:26 +0900

    Step04 finished

--- Step04_JspBasic/src/test/servlet/FortuneServlet.java
+++ Step04_JspBasic/src/test/servlet/FortuneServlet.java
@@ -17,7 +17,8 @@ public class FortuneServlet extends HttpServlet{
 		
 		// 응답을 jsp 페이지 혹은 다른 서블릿으로 위임할 수 있다.
 		
-		// 1. 응답을 위임할 이동 경로
+		// 1. 응답을 위임할 이동 경로 (context path는 넣지 않는다)
+		// (context path 넣지 않는 이유 : 포워드 이동은 같은 컨텍스트(프로젝트) 내에서만 이동 가능하기 때문
 		String path="/test2/person.jsp";
 		// 2. RequestDispatcher 객체의 참조값
 		RequestDispatcher rd=req.getRequestDispatcher(path);
